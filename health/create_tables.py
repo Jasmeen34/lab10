b084aad99a4e37d8f705ac907ed12810bd1559cf
Jasmeen34 <jasmeenkaurgill2002@gmail.com>
2022-11-25 03:03:52 +0000

    changes

---
+++ health/create_tables.py
@@ -0,0 +1,17 @@
+import sqlite3
+import datetime
+
+conn = sqlite3.connect('health_status.sqlite')
+
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE health_status
+    (id INTEGER PRIMARY KEY ASC,
+    receiver STRING(50) NOT NULL,
+    storage  STRING(50) NOT NULL,
+    processing STRING(100) NOT NULL,
+    aduit STRING(100) NOT NULL,
+    last_updated STRING(100) NOT NULL)
+''')
+conn.commit()
+conn.close()
